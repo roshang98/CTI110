7026d7fd9e4e4ace3534edc3c3b57adc28f752dd
roshang98 <43101653+roshang98@users.noreply.github.com>
2018-09-19 15:31:57 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_RoshanChan.py
@@ -0,0 +1,17 @@
+# sales prediction
+# 09/19/18
+# CTI-110 P2T1 - Sales Prediction
+# Roshan Chandrasekara
+#Enter Total sales amount
+
+SaleAmount=float(input('Enter Total Sales Amount $: ',))
+#calculate the profit
+Profit_amount=(SaleAmount*23/100)
+#display the profit amount
+print('Profit Amount $=',format(Profit_amount,'.2f'))
+input()
+
+
+
+
+
